ee6c49152ff4a9b89d248438bd9093449294956a
Pattanapong <pattanapongua@gmail.com>
2024-08-21 14:56:41 +0700

    set filea

--- src/main/java/com/example/exam/controller/UserController.java
+++ src/main/java/com/example/exam/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.example.exam.controller;
 
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.http.ContentDisposition;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -25,27 +26,25 @@ public class UserController {
     @GetMapping("download")
     public void download(HttpServletRequest request, HttpServletResponse response) throws IOException, URISyntaxException {
 
-
-//        InputStream inputStream = classPathResource.getInputStream();
-        Path file = Path.of(ClassLoader.getSystemResource("report/sample.pdf").toURI());
+        Resource resource = new ClassPathResource("report/sample.pdf");
 
         // Get the media type of the file
-        String contentType = Files.probeContentType(file);
+        String contentType = Files.probeContentType(resource.getFile().toPath());
         if (contentType == null) {
             // Use the default media type
             contentType = MediaType.APPLICATION_OCTET_STREAM_VALUE;
         }
         response.setContentType(contentType);
         // File Size
-        response.setContentLengthLong(Files.size(file));
+        response.setContentLengthLong(Files.size(resource.getFile().toPath()));
         /**
          * Building the Content-Disposition header with the ContentDisposition utility class can avoid the problem of garbled downloaded file names.
          */
         response.setHeader(HttpHeaders.CONTENT_DISPOSITION, ContentDisposition.attachment()
-                .filename(file.getFileName().toString(), StandardCharsets.UTF_8)
+                .filename(resource.getFile().toPath().getFileName().toString(), StandardCharsets.UTF_8)
                 .build()
                 .toString());
         // Response data to the client
-        Files.copy(file, response.getOutputStream());
+        Files.copy(resource.getFile().toPath(), response.getOutputStream());
     }
 }
