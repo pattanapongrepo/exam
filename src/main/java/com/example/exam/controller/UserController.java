d4f329c94bc4eb79d37b0b2cd1c1f521fe37f84c
Pattanapong <pattanapongua@gmail.com>
2024-08-21 15:44:13 +0700

    set filea

--- src/main/java/com/example/exam/controller/UserController.java
+++ src/main/java/com/example/exam/controller/UserController.java
@@ -1,51 +1,44 @@
 package com.example.exam.controller;
 
-import org.springframework.core.io.ClassPathResource;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.tomcat.util.http.fileupload.IOUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
 import org.springframework.core.io.Resource;
-import org.springframework.http.ContentDisposition;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.util.ResourceUtils;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.net.URISyntaxException;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 
+@Slf4j
 @RestController
 @RequestMapping("/users")
 public class UserController {
+
+    @Autowired
+    ResourceLoader resourceLoader;
+
+
     @GetMapping("download")
     public void download(HttpServletRequest request, HttpServletResponse response) throws IOException, URISyntaxException {
+        log.info("{}","sdadasda");
+        Resource resource = resourceLoader.getResource("classpath:report/sample.pdf");
+// Get your file stream from wherever.
+        InputStream myStream = resource.getInputStream();
+        // Set the content type and attachment header.
+        response.addHeader("Content-disposition", "attachment;filename=sample.pdf");
+        response.setContentType(MediaType.APPLICATION_OCTET_STREAM_VALUE);
 
-        File file = ResourceUtils.getFile("classpath:report/sample.pdf");
-
-        // Get the media type of the file
-        String contentType = Files.probeContentType(file.toPath());
-        if (contentType == null) {
-            // Use the default media type
-            contentType = MediaType.APPLICATION_OCTET_STREAM_VALUE;
-        }
-        response.setContentType(contentType);
-        // File Size
-        response.setContentLengthLong(Files.size(file.toPath()));
-        /**
-         * Building the Content-Disposition header with the ContentDisposition utility class can avoid the problem of garbled downloaded file names.
-         */
-        response.setHeader(HttpHeaders.CONTENT_DISPOSITION, ContentDisposition.attachment()
-                .filename(file.toPath().getFileName().toString(), StandardCharsets.UTF_8)
-                .build()
-                .toString());
-        // Response data to the client
-        Files.copy(file.toPath(), response.getOutputStream());
+        // Copy the stream to the response's output stream.
+        IOUtils.copy(myStream, response.getOutputStream());
+        response.flushBuffer();
     }
 }
